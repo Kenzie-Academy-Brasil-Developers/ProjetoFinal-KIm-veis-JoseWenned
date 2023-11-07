ce9c8d029edc14389a937bb97e7285d6cebe7d19
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2023-11-07 11:32:33 -0300

    schemas read

---
+++ src/interfaces/categories.interface.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+import { createCategorySchema, readAllCategoriesSchema,  } from "../schemas/categories.schema";
+import { Repository } from "typeorm";
+import Category from "../entities/category.entities";
+
+export type CreateCategory = z.infer<typeof createCategorySchema>;
+
+export type ReadAllCategory = z.infer<typeof readAllCategoriesSchema>;
+
+export type CategoryRepo = Repository<Category>;
---
+++ src/interfaces/users.interfaces.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import { createUserSchema, userLoginSchema, userReturnSchema } from "../schemas/users.schema";
+import { DeepPartial, Repository } from "typeorm";
+import { User } from "../entities/index.entities";
+
+export type UserCreate = z.infer<typeof createUserSchema>;
+
+export type UserBodyUpdate = Omit<UserCreate, "admin">;
+
+export type UserUpdate = DeepPartial<UserBodyUpdate>;
+
+export type UserReturn = z.infer<typeof userReturnSchema>;
+
+export type UserLogin = z.infer<typeof userLoginSchema>;
+
+export type ReturnLogin = {token: string};
+
+export type UserRepo = Repository<User>;
