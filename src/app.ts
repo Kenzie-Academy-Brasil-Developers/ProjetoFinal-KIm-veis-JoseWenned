16e327f26f7cc2927d979f086048f7c8f6aa700e
unknown <wennedchaves1997@gmail.com>
2024-04-18 11:57:10 -0300

    feat: concluded document swagger api the project application

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import 'express-async-errors';
 import  express, {Application, json}  from 'express';
 import { routes } from './routers';
 import { handleErrors } from './middlewares/handleErrors.middleware';
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerDocument from "./swagger.json"
 
 const app: Application = express();
 
 app.use(json());
 app.use("/", routes);
 app.use(handleErrors);
+app.use("/api-documentation", swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocument))
 
 export default app;
